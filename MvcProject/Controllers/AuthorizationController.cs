e7c5c229d736d466e3fc3f9a006c911363d61ae8
zeynepdaglar <86071617+zeynepdaglar@users.noreply.github.com>
2022-01-12 12:34:03 +0300

    AddAdmin

--- MvcProject/Controllers/AuthorizationController.cs
+++ MvcProject/Controllers/AuthorizationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,19 @@ namespace MvcProject.Controllers
         {
             var adminvalues = adminManager.GetList();
             return View(adminvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAdmin(Admin parameter)
+        {
+            adminManager.AdminAddBL(parameter);
+            return View("Index");
         }
     }
 }
