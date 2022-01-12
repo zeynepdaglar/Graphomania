27652840b21d6fa2d8632b9512f237ff0a3cb1f9
zeynepdaglar <86071617+zeynepdaglar@users.noreply.github.com>
2022-01-12 11:47:25 +0300

    IAdminDAL

--- DataAccessLayer/Abstract/IAdminDAL.cs
+++ DataAccessLayer/Abstract/IAdminDAL.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    class IAdminDAL
+    public class IAdminDAL : IRepository<Admin>
     {
+        IAdminDAL _adminDAL;
+        public void Delete(Admin p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Admin Get(Expression<Func<Admin, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Admin p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Admin> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Admin> List(Expression<Func<Admin, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Admin p)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
